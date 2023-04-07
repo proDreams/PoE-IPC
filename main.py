b60882d39e4aba5d8660e1090e6775fe284b793b
Ivan Ashikhmin <sushkoos@gmail.com>
2023-04-07 17:22:05 +0400

    Сделал простейший логгер для записи ошибок в файл

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from app.controller import AppMenu
+import logging
 
 if __name__ == "__main__":
-    AppMenu()
+    logging.basicConfig(filename="log.log",
+                        format="%(asctime)s - %(levelname)s: %(lineno)d - %(message)s")
+    try:
+        AppMenu()
+    except BaseException:
+        logging.exception("An exception was thrown!")
+        print("Произошла ошибка. Свяжитесь с автором для решения.\n"
+              "Error was found. Please contact with Author.")
